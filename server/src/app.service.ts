7450838cfe753c4cd21bc63e2d571a4d028e2b93
goldinof <goldinof@protonmail.com>
2022-09-28 17:29:23 +0200

    updated

--- server/src/app.service.ts
+++ server/src/app.service.ts
@@ -1,15 +1,13 @@
-import { Injectable, MessageEvent } from '@nestjs/common';
-import { EventEmitter2 } from '@nestjs/event-emitter';
-import { from, fromEvent, Observable, Subject } from 'rxjs';
+import { Injectable, MessageEvent } from "@nestjs/common";
+import { Observable, Subject } from "rxjs";
 
 @Injectable()
 export class AppService {
-  constructor(private readonly eventEmitter: EventEmitter2) {}
   private events = new Subject<MessageEvent>();
 
   getHello(): string {
-    this.events.next({ data: 'new step' });
-    return 'Hello World!';
+    this.events.next({ data: "next step" });
+    return "Hello Monaco!";
   }
   subscribe(): Observable<MessageEvent> {
     return this.events.asObservable();
